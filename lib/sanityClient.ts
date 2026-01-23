2bf230ace57918fb03cec2ffd257999dc8337678
GregDjian <tgreg.pro@gmail.com>
2026-01-24 00:01:23 +0400

    Update sanityClient apiVersion

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url';
 export const sanityClient = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+  apiVersion: "2024-01-01",
   useCdn: true, // fast + cheap
 });
 
